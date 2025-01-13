c042b54ab69ba0aa955b0a288884035fe89c3961
Aaron Wang <aaronwang04@icloud.com>
2025-01-13 12:31:58 -0500

    bug fix 2?

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,8 @@ import Board from '../pages/api/board';
 import { Board as BoardType, BoardMongo } from '../types/Board';
 import Main from './src/components/Main';
 
+export const fetchCache = 'force-no-store';
+
 async function fetchBoards(): Promise<BoardType[]> {
   // Ensure the MongoDB connection is established
   if (!mongoose.connections[0].readyState) {
