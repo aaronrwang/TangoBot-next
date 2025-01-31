8903bb48f9e92895c2ade4901212b17a8701576f
Aaron Wang <aaronwang04@icloud.com>
2025-01-31 03:28:05 -0500

    difficulty indicator

--- app/src/components/Main.tsx
+++ app/src/components/Main.tsx
@@ -14,6 +14,19 @@ const Main = ({ boards }: MainProps) => {
 
   // Set the initial board state to the first sorted board
   const [board, setBoard] = useState<BoardType | null>(sortedBoards[0] || null);
+  let hardmoves = board?.moves.map(move => Number(move[2][0]));
+  let difficultyCount = hardmoves?.reduce<number>((acc, num) => acc + num, 0);
+  let difficulty = ''
+  if (difficultyCount != undefined) {
+    if (difficultyCount >= 65) {
+      difficulty = 'Hard'
+    } else if (difficultyCount >= 50) {
+      difficulty = 'Medium'
+    } else {
+      difficulty = 'Easy'
+    }
+  }
+  console.log(difficulty + ': ' + difficultyCount)
 
   const dateRef = useRef<HTMLSelectElement>(null);
 
@@ -36,9 +49,9 @@ const Main = ({ boards }: MainProps) => {
 
   return (
     <div className="w-screen h-screen md:px-8 md:py-4">
-      <div className="flex w-full gap-4 my-4 mx-8">
-        <label className="align-middle" htmlFor="date-select">
-          Select a Date:
+      <div className="flex w-full gap-4 my-4 mx-2 md:mx-8">
+        <label className="md:text-2xl" htmlFor="date-select">
+          Select Date:
         </label>
         <select id="date-select" name="dates" ref={dateRef} defaultValue={dates[0]}>
           {dates.map((date) => (
@@ -53,6 +66,9 @@ const Main = ({ boards }: MainProps) => {
         >
           Submit
         </button>
+        <label className={`md:text-2xl ${difficulty === "Hard" ? "text-red-500" : difficulty === "Medium" ? "text-yellow-500" : "text-green-500"}`}>
+          {difficulty}
+        </label>
       </div>
       {board ? <Game boardOrig={board} /> : <div>Loading...</div>}
     </div>
